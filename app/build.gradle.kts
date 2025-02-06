556719ff75c11d33311681faa45ca2a1dcce4923
supershadoe <supershadoe@proton.me>
2025-02-06 23:05:15 +0530

    Update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "dev.shadoe.delta"
         minSdk = 30
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.1.4"
+        versionCode = 5
+        versionName = "0.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
