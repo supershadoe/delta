0a2b2360baf9929c65efe58b40a009c09aa85837
supershadoe <supershadoe@proton.me>
2025-02-16 03:21:39 +0530

    allow only one change at a time

--- app/src/main/java/dev/shadoe/delta/hotspot/HotspotEditScreen.kt
+++ app/src/main/java/dev/shadoe/delta/hotspot/HotspotEditScreen.kt
@@ -1,10 +1,12 @@
 package dev.shadoe.delta.hotspot
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardActions
@@ -14,6 +16,7 @@ import androidx.compose.material.icons.automirrored.rounded.ArrowBack
 import androidx.compose.material.icons.rounded.Password
 import androidx.compose.material.icons.rounded.Wifi
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -23,7 +26,9 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -35,7 +40,9 @@ import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import dev.shadoe.delta.hotspot.navigation.LocalNavController
+import dev.shadoe.hotspotapi.WifiApEnabledStates
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
@@ -43,9 +50,32 @@ import kotlinx.coroutines.runBlocking
 @Composable
 fun HotspotEditScreen() {
     val navController = LocalNavController.current
+
     val hotspotApi = LocalHotspotApiInstance.current!!
+    val enabledState = hotspotApi.enabledState.collectAsState()
     val ssid = hotspotApi.ssid.collectAsState(null)
     val password = hotspotApi.passphrase.collectAsState(null)
+
+    val allowEdits = remember { mutableStateOf(true) }
+    val updateCounter = remember { mutableIntStateOf(0) }
+
+    LaunchedEffect(updateCounter.intValue) {
+        // TODO: consider using a button for applying all changes at once
+        // instead of this bs
+        if (updateCounter.intValue == 0) return@LaunchedEffect
+        if (enabledState.value == WifiApEnabledStates.WIFI_AP_STATE_ENABLED) {
+            allowEdits.value = false
+            hotspotApi.stopHotspot()
+            while (enabledState.value != WifiApEnabledStates.WIFI_AP_STATE_DISABLED) {
+                delay(500)
+            }
+            hotspotApi.startHotspot()
+            while (enabledState.value != WifiApEnabledStates.WIFI_AP_STATE_ENABLED) {
+                delay(500)
+            }
+            allowEdits.value = true
+        }
+    }
 
     Scaffold(
         topBar = {
@@ -61,25 +91,44 @@ fun HotspotEditScreen() {
                 },
             )
         }) { scaffoldPadding ->
-        Column(
-            modifier = Modifier
-                .fillMaxHeight()
-                .padding(scaffoldPadding)
-        ) {
-            EditWidget(
-                onClickLabel = "Edit SSID",
-                icon = Icons.Rounded.Wifi,
-                text = "SSID",
-                value = ssid.value ?: "",
-                onSave = { hotspotApi.setSsid(it) })
-            EditWidget(
-                onClickLabel = "Edit Password",
-                icon = Icons.Rounded.Password,
-                text = "Password",
-                maskValue = true,
-                value = password.value ?: "",
-                onSave = { hotspotApi.setPassphrase(it) },
-            )
+        if (allowEdits.value) {
+            Column(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .padding(scaffoldPadding)
+            ) {
+                EditWidget(
+                    onClickLabel = "Edit SSID",
+                    icon = Icons.Rounded.Wifi,
+                    text = "SSID",
+                    value = ssid.value ?: "",
+                    onUpdate = { updateCounter.intValue++ },
+                    onSave = { hotspotApi.setSsid(it) },
+                )
+                EditWidget(
+                    onClickLabel = "Edit Password",
+                    icon = Icons.Rounded.Password,
+                    text = "Password",
+                    maskValue = true,
+                    value = password.value ?: "",
+                    onUpdate = { updateCounter.intValue++ },
+                    onSave = { hotspotApi.setPassphrase(it) },
+                )
+            }
+        } else {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(scaffoldPadding),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+            ) {
+                CircularProgressIndicator()
+                Text(
+                    text = "Updating settings…",
+                    modifier = Modifier.padding(top = 8.dp),
+                )
+            }
         }
     }
 }
@@ -90,6 +139,7 @@ private fun EditWidget(
     icon: ImageVector,
     text: String,
     value: String,
+    onUpdate: () -> Unit,
     maskValue: Boolean = false,
     onSave: (String) -> Unit = {},
 ) {
@@ -102,6 +152,7 @@ private fun EditWidget(
             }
         }
         isEditing.value = false
+        onUpdate()
     }
     Box(
         modifier = Modifier
