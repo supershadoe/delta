658c39c1f976f18e7ceb3957dfd36f2a150bbabc
supershadoe <supershadoe@proton.me>
2025-02-17 20:55:24 +0530

    Actually query for the last used password :person_facepalming:

--- hotspot-api/src/main/java/dev/shadoe/hotspotapi/HotspotApi.kt
+++ hotspot-api/src/main/java/dev/shadoe/hotspotapi/HotspotApi.kt
@@ -58,24 +58,24 @@ class HotspotApi(
     companion object {
         private const val ADB_PACKAGE_NAME = "com.android.shell"
 
+        /*
+         * Copyright (C) 2023 The Android Open Source Project
+         *
+         * Licensed under the Apache License, Version 2.0 (the "License");
+         * you may not use this file except in compliance with the License.
+         * You may obtain a copy of the License at
+         *
+         *      http://www.apache.org/licenses/LICENSE-2.0
+         *
+         * Unless required by applicable law or agreed to in writing, software
+         * distributed under the License is distributed on an "AS IS" BASIS,
+         * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+         * See the License for the specific language governing permissions and
+         * limitations under the License.
+         *
+         * From https://cs.android.com/android/platform/superproject/main/+/29fbb69343c063b65d71180d04f5d2acaf4f050c:packages/apps/Settings/src/com/android/settings/wifi/repository/WifiHotspotRepository.java;l=168
+         */
         private fun generateRandomPassword(): String {
-            /*
-             * Copyright (C) 2023 The Android Open Source Project
-             *
-             * Licensed under the Apache License, Version 2.0 (the "License");
-             * you may not use this file except in compliance with the License.
-             * You may obtain a copy of the License at
-             *
-             *      http://www.apache.org/licenses/LICENSE-2.0
-             *
-             * Unless required by applicable law or agreed to in writing, software
-             * distributed under the License is distributed on an "AS IS" BASIS,
-             * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-             * See the License for the specific language governing permissions and
-             * limitations under the License.
-             *
-             * From https://cs.android.com/android/platform/superproject/main/+/29fbb69343c063b65d71180d04f5d2acaf4f050c:packages/apps/Settings/src/com/android/settings/wifi/repository/WifiHotspotRepository.java;l=168
-             */
             val randomUUID = UUID.randomUUID().toString()
             //first 12 chars from xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx
             return randomUUID.substring(0, 8) + randomUUID.substring(9, 13)
@@ -158,9 +158,11 @@ class HotspotApi(
 
     private fun updateSoftApConfigurationHidden(conf: SoftApConfigurationHidden): Boolean {
         if (!wifiManager.validateSoftApConfiguration(
-                Refine.unsafeCast<SoftApConfiguration>(conf)
+                Refine.unsafeCast<SoftApConfiguration>(conf),
             )
-        ) return false
+        ) {
+            return false
+        }
         _softApConfiguration.value = conf
         wifiManager.setSoftApConfiguration(
             Refine.unsafeCast<SoftApConfiguration>(
@@ -254,4 +256,9 @@ class HotspotApi(
             TetheringResultListener(StopTetheringCallback()),
         )
     }
+
+    fun queryLastUsedPassphraseSinceBoot() =
+        wifiManager.queryLastConfiguredTetheredApPassphraseSinceBoot(
+            lastPassphraseListener
+        )
 }
