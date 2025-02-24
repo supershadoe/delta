5e51d6b994034ee7ed5496c6b1eed1f0ec02e625
supershadoe <supershadoe@proton.me>
2025-02-24 23:14:13 +0530

    Add compose-rules to ktlint
    To enforce some rules on Composables alone

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,11 @@ spotless {
         target("**/*.kt", "**/*.kts")
         targetExclude("**/build/**/*.kt")
         ktlint("1.5.0")
+            .customRuleSets(
+                listOf(
+                    "io.nlopez.compose.rules:ktlint:0.4.22",
+                ),
+            )
         trimTrailingWhitespace()
         endWithNewline()
     }
