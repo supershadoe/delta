43df7fe0290db2d332902e78b77e5e1b3305f6dc
supershadoe <supershadoe@proton.me>
2025-02-26 21:44:47 +0530

    Add nunito sans font to app

---
+++ app/src/main/kotlin/dev/shadoe/delta/typography/Typography.kt
@@ -0,0 +1,71 @@
+package dev.shadoe.delta.typography
+
+import android.annotation.SuppressLint
+import android.os.Build
+import androidx.compose.material3.Typography
+
+object Typography {
+    private val defaultTypography = Typography()
+
+    // Potentially will backport app < SDK 30
+    // Marked as to do elsewhere in [VariableFontFamily]
+    @SuppressLint("ObsoleteSdkInt")
+    private val fontFamily =
+        if (Build.VERSION.SDK_INT >=
+            Build.VERSION_CODES.O
+        ) {
+            VariableFontFamily
+        } else {
+            StaticFontFamily
+        }
+
+    private val displayLarge =
+        defaultTypography.displayLarge.copy(fontFamily = fontFamily)
+    private val displayMedium =
+        defaultTypography.displayMedium.copy(fontFamily = fontFamily)
+    private val displaySmall =
+        defaultTypography.displaySmall.copy(fontFamily = fontFamily)
+    private val headlineLarge =
+        defaultTypography.headlineLarge.copy(fontFamily = fontFamily)
+    private val headlineMedium =
+        defaultTypography.headlineMedium.copy(fontFamily = fontFamily)
+    private val headlineSmall =
+        defaultTypography.headlineSmall.copy(fontFamily = fontFamily)
+    private val titleLarge =
+        defaultTypography.titleLarge.copy(fontFamily = fontFamily)
+    private val titleMedium =
+        defaultTypography.titleMedium.copy(fontFamily = fontFamily)
+    private val titleSmall =
+        defaultTypography.titleSmall.copy(fontFamily = fontFamily)
+    private val bodyLarge =
+        defaultTypography.bodyLarge.copy(fontFamily = fontFamily)
+    private val bodyMedium =
+        defaultTypography.bodyMedium.copy(fontFamily = fontFamily)
+    private val bodySmall =
+        defaultTypography.bodySmall.copy(fontFamily = fontFamily)
+    private val labelLarge =
+        defaultTypography.labelLarge.copy(fontFamily = fontFamily)
+    private val labelMedium =
+        defaultTypography.labelMedium.copy(fontFamily = fontFamily)
+    private val labelSmall =
+        defaultTypography.labelSmall.copy(fontFamily = fontFamily)
+
+    val value =
+        Typography(
+            displayLarge = displayLarge,
+            displayMedium = displayMedium,
+            displaySmall = displaySmall,
+            headlineLarge = headlineLarge,
+            headlineMedium = headlineMedium,
+            headlineSmall = headlineSmall,
+            titleLarge = titleLarge,
+            titleMedium = titleMedium,
+            titleSmall = titleSmall,
+            bodyLarge = bodyLarge,
+            bodyMedium = bodyMedium,
+            bodySmall = bodySmall,
+            labelLarge = labelLarge,
+            labelMedium = labelMedium,
+            labelSmall = labelSmall,
+        )
+}
---
+++ app/src/main/kotlin/dev/shadoe/delta/typography/VariableFontFamily.kt
@@ -0,0 +1,156 @@
+package dev.shadoe.delta.typography
+
+import android.annotation.SuppressLint
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.ui.text.ExperimentalTextApi
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontVariation
+import androidx.compose.ui.text.font.FontWeight
+import dev.shadoe.delta.R
+
+// TODO: Potentially will backport app < SDK 30
+@SuppressLint("ObsoleteSdkInt")
+@OptIn(ExperimentalTextApi::class)
+@RequiresApi(Build.VERSION_CODES.O)
+internal val VariableFontFamily =
+    FontFamily(
+        Font(
+            R.font.nunitosans_variable,
+            weight = FontWeight.W200,
+            variationSettings =
+                FontVariation.Settings(
+                    FontVariation.weight(FontWeight.W200.weight),
+                ),
+        ),
+        Font(
+            R.font.nunitosans_variable,
+            weight = FontWeight.W300,
+            variationSettings =
+                FontVariation.Settings(
+                    FontVariation.weight(FontWeight.W300.weight),
+                ),
+        ),
+        Font(
+            R.font.nunitosans_variable,
+            weight = FontWeight.W400,
+            variationSettings =
+                FontVariation.Settings(
+                    FontVariation.weight(FontWeight.W400.weight),
+                ),
+        ),
+        Font(
+            R.font.nunitosans_variable,
+            weight = FontWeight.W500,
+            variationSettings =
+                FontVariation.Settings(
+                    FontVariation.weight(FontWeight.W500.weight),
+                ),
+        ),
+        Font(
+            R.font.nunitosans_variable,
+            weight = FontWeight.W600,
+            variationSettings =
+                FontVariation.Settings(
+                    FontVariation.weight(FontWeight.W600.weight),
+                ),
+        ),
+        Font(
+            R.font.nunitosans_variable,
+            weight = FontWeight.W700,
+            variationSettings =
+                FontVariation.Settings(
+                    FontVariation.weight(FontWeight.W700.weight),
+                ),
+        ),
+        Font(
+            R.font.nunitosans_variable,
+            weight = FontWeight.W800,
+            variationSettings =
+                FontVariation.Settings(
+                    FontVariation.weight(FontWeight.W800.weight),
+                ),
+        ),
+        Font(
+            R.font.nunitosans_variable,
+            weight = FontWeight.W900,
+            variationSettings =
+                FontVariation.Settings(
+                    FontVariation.weight(FontWeight.W900.weight),
+                ),
+        ),
+        Font(
+            R.font.nunitosans_italic_variable,
+            weight = FontWeight.W200,
+            style = FontStyle.Italic,
+            variationSettings =
+                FontVariation.Settings(
+                    FontVariation.weight(FontWeight.W200.weight),
+                ),
+        ),
+        Font(
+            R.font.nunitosans_italic_variable,
+            weight = FontWeight.W300,
+            style = FontStyle.Italic,
+            variationSettings =
+                FontVariation.Settings(
+                    FontVariation.weight(FontWeight.W300.weight),
+                ),
+        ),
+        Font(
+            R.font.nunitosans_italic_variable,
+            weight = FontWeight.W400,
+            style = FontStyle.Italic,
+            variationSettings =
+                FontVariation.Settings(
+                    FontVariation.weight(FontWeight.W400.weight),
+                ),
+        ),
+        Font(
+            R.font.nunitosans_italic_variable,
+            weight = FontWeight.W500,
+            style = FontStyle.Italic,
+            variationSettings =
+                FontVariation.Settings(
+                    FontVariation.weight(FontWeight.W500.weight),
+                ),
+        ),
+        Font(
+            R.font.nunitosans_italic_variable,
+            weight = FontWeight.W600,
+            style = FontStyle.Italic,
+            variationSettings =
+                FontVariation.Settings(
+                    FontVariation.weight(FontWeight.W600.weight),
+                ),
+        ),
+        Font(
+            R.font.nunitosans_italic_variable,
+            weight = FontWeight.W700,
+            style = FontStyle.Italic,
+            variationSettings =
+                FontVariation.Settings(
+                    FontVariation.weight(FontWeight.W700.weight),
+                ),
+        ),
+        Font(
+            R.font.nunitosans_italic_variable,
+            weight = FontWeight.W800,
+            style = FontStyle.Italic,
+            variationSettings =
+                FontVariation.Settings(
+                    FontVariation.weight(FontWeight.W800.weight),
+                ),
+        ),
+        Font(
+            R.font.nunitosans_italic_variable,
+            weight = FontWeight.W900,
+            style = FontStyle.Italic,
+            variationSettings =
+                FontVariation.Settings(
+                    FontVariation.weight(FontWeight.W900.weight),
+                ),
+        ),
+    )
