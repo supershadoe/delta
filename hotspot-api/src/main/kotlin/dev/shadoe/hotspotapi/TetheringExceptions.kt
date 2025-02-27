1fc83e7dceb35850717dbcaf35e3122628da414d
supershadoe <supershadoe@proton.me>
2025-02-28 01:04:26 +0530

    Move exceptions to its own package

--- hotspot-api/src/main/kotlin/dev/shadoe/hotspotapi/TetheringExceptions.kt
+++
@@ -1,7 +0,0 @@
-package dev.shadoe.hotspotapi
-
-class TetheringExceptions private constructor() {
-    class BinderAcquisitionException(
-        message: String,
-    ) : Exception(message)
-}
---
+++ hotspot-api/src/main/kotlin/dev/shadoe/hotspotapi/exceptions/BinderAcquisitionException.kt
@@ -0,0 +1,5 @@
+package dev.shadoe.hotspotapi.exceptions
+
+class BinderAcquisitionException(
+    message: String,
+) : Exception(message)
