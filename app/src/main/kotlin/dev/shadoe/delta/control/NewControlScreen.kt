9927ec762fa97b6cb667893ae01158c213f1980b
supershadoe <shadoe@shadoe.dev>
2025-04-18 23:21:30 +0530

    Constrain UI by width
    It makes no sense to render edge to edge on very large devices, harder to find UI elements.
    
    It's also harder to design for random sizes without having access to such devices.

--- app/src/main/kotlin/dev/shadoe/delta/control/NewControlScreen.kt
+++ app/src/main/kotlin/dev/shadoe/delta/control/NewControlScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -93,225 +94,230 @@ fun NewControlScreen(
     }
   }
 
-  Scaffold(
-    topBar = {
-      @OptIn(ExperimentalMaterial3Api::class)
-      CenterAlignedTopAppBar(
-        title = { AppBarWithDebugAction(onNavigateToDebug) }
-      )
-    }
-  ) { scaffoldPadding ->
-    LazyColumn(modifier = Modifier.padding(scaffoldPadding)) {
-      item {
-        Row(
-          modifier =
-            Modifier.fillMaxWidth()
-              .padding(horizontal = 16.dp)
-              .clip(RoundedCornerShape(8.dp))
-              .background(MaterialTheme.colorScheme.primaryContainer),
-          verticalAlignment = Alignment.CenterVertically,
-          horizontalArrangement = Arrangement.SpaceBetween,
-        ) {
-          Column(
-            modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp),
-            verticalArrangement = Arrangement.Center,
+  Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
+    Scaffold(
+      topBar = {
+        @OptIn(ExperimentalMaterial3Api::class)
+        CenterAlignedTopAppBar(
+          title = { AppBarWithDebugAction(onNavigateToDebug) }
+        )
+      },
+      modifier = Modifier.widthIn(max = 600.dp),
+    ) { scaffoldPadding ->
+      LazyColumn(modifier = Modifier.padding(scaffoldPadding)) {
+        item {
+          Row(
+            modifier =
+              Modifier.fillMaxWidth()
+                .padding(horizontal = 16.dp)
+                .clip(RoundedCornerShape(8.dp))
+                .background(MaterialTheme.colorScheme.primaryContainer),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
           ) {
-            Row(verticalAlignment = Alignment.CenterVertically) {
-              Icon(
-                imageVector = Icons.Rounded.Wifi,
-                contentDescription = stringResource(R.string.ssid_field_icon),
-                modifier = Modifier.padding(end = 8.dp),
-              )
-              Text(text = ssid ?: stringResource(id = R.string.no_ssid))
-            }
-            if (shouldShowPassphrase) {
-              Row(
-                modifier = Modifier.padding(vertical = 8.dp),
-                verticalAlignment = Alignment.CenterVertically,
-              ) {
+            Column(
+              modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp),
+              verticalArrangement = Arrangement.Center,
+            ) {
+              Row(verticalAlignment = Alignment.CenterVertically) {
                 Icon(
-                  imageVector = Icons.Rounded.Password,
-                  contentDescription =
-                    stringResource(R.string.passphrase_field_icon),
+                  imageVector = Icons.Rounded.Wifi,
+                  contentDescription = stringResource(R.string.ssid_field_icon),
                   modifier = Modifier.padding(end = 8.dp),
                 )
-                PassphraseDisplay(passphrase = passphrase)
+                Text(text = ssid ?: stringResource(id = R.string.no_ssid))
+              }
+              if (shouldShowPassphrase) {
+                Row(
+                  modifier = Modifier.padding(vertical = 8.dp),
+                  verticalAlignment = Alignment.CenterVertically,
+                ) {
+                  Icon(
+                    imageVector = Icons.Rounded.Password,
+                    contentDescription =
+                      stringResource(R.string.passphrase_field_icon),
+                    modifier = Modifier.padding(end = 8.dp),
+                  )
+                  PassphraseDisplay(passphrase = passphrase)
+                }
               }
             }
-          }
-          Row(verticalAlignment = Alignment.CenterVertically) {
-            if (shouldShowQrButton) {
-              IconButton(
-                onClick = {
-                  if (
-                    !vm.openQrCodeScreen(
-                      context,
-                      windowSizeClass.windowWidthSizeClass ==
-                        WindowWidthSizeClass.EXPANDED,
-                    )
-                  ) {
-                    scope.launch {
-                      snackbarHostState.showSnackbar(
-                        message = stringFeatureNotSupported,
-                        withDismissAction = true,
-                        duration = SnackbarDuration.Short,
+            Row(verticalAlignment = Alignment.CenterVertically) {
+              if (shouldShowQrButton) {
+                IconButton(
+                  onClick = {
+                    if (
+                      !vm.openQrCodeScreen(
+                        context,
+                        windowSizeClass.windowWidthSizeClass ==
+                          WindowWidthSizeClass.EXPANDED,
                       )
+                    ) {
+                      scope.launch {
+                        snackbarHostState.showSnackbar(
+                          message = stringFeatureNotSupported,
+                          withDismissAction = true,
+                          duration = SnackbarDuration.Short,
+                        )
+                      }
                     }
                   }
-                }
-              ) {
-                Icon(
-                  imageVector = Icons.Rounded.QrCode2,
-                  contentDescription =
-                    stringResource(id = R.string.qr_code_button),
-                  modifier = Modifier.size(48.dp),
-                )
-              }
-            }
-            SoftApControlButton(
-              enabledState,
-              startHotspot = { vm.startHotspot() },
-              stopHotspot = { vm.stopHotspot() },
-            )
-          }
-        }
-      }
-      item {
-        Text(
-          text = stringResource(R.string.connected_devices),
-          color = MaterialTheme.colorScheme.onSurfaceVariant,
-          style = MaterialTheme.typography.titleLarge,
-          modifier = Modifier.padding(16.dp),
-        )
-      }
-      if (tetheredClients.isEmpty()) {
-        item {
-          Box(
-            modifier = Modifier.fillMaxWidth(),
-            contentAlignment = Alignment.Center,
-          ) {
-            Text(text = stringResource(R.string.no_connected_devices))
-          }
-        }
-      }
-      items(tetheredClients) { client ->
-        Row(
-          modifier =
-            Modifier.fillMaxWidth()
-              .padding(horizontal = 16.dp, vertical = 4.dp),
-          horizontalArrangement = Arrangement.SpaceBetween,
-          verticalAlignment = Alignment.CenterVertically,
-        ) {
-          Column(modifier = Modifier.padding(horizontal = 8.dp)) {
-            Text(
-              text =
-                client.hostname ?: stringResource(R.string.no_client_hostname)
-            )
-
-            client.address?.address?.hostAddress.let { ip ->
-              Row(
-                horizontalArrangement = Arrangement.spacedBy(8.dp),
-                verticalAlignment = Alignment.CenterVertically,
-                modifier =
-                  Modifier.clickable {
-                    ip ?: return@clickable
-                    clipboardManager.setText(AnnotatedString(ip))
-                  },
-              ) {
-                Text(text = ip ?: stringResource(R.string.ip_not_allocated))
-
-                if (ip != null) {
+                ) {
                   Icon(
-                    imageVector = Icons.Rounded.ContentCopy,
-                    contentDescription = stringResource(R.string.copy_button),
-                    tint = MaterialTheme.colorScheme.primary,
-                    modifier =
-                      Modifier.size(
-                        with(density) { LocalTextStyle.current.fontSize.toDp() }
-                      ),
+                    imageVector = Icons.Rounded.QrCode2,
+                    contentDescription =
+                      stringResource(id = R.string.qr_code_button),
+                    modifier = Modifier.size(48.dp),
                   )
                 }
               }
-            }
-          }
-          Button(
-            onClick = {
-              vm.blockDevice(
-                device =
-                  ACLDevice(
-                    hostname = client.hostname,
-                    macAddress = client.macAddress,
-                  )
+              SoftApControlButton(
+                enabledState,
+                startHotspot = { vm.startHotspot() },
+                stopHotspot = { vm.stopHotspot() },
               )
             }
-          ) {
-            Text(text = stringResource(R.string.block_button))
           }
         }
-      }
-      item {
-        Row(
-          modifier =
-            Modifier.fillMaxWidth()
-              .clickable { isBlocklistShown = !isBlocklistShown }
-              .padding(16.dp),
-          horizontalArrangement = Arrangement.SpaceBetween,
-        ) {
+        item {
           Text(
-            text = stringResource(R.string.blocklist),
+            text = stringResource(R.string.connected_devices),
             color = MaterialTheme.colorScheme.onSurfaceVariant,
             style = MaterialTheme.typography.titleLarge,
-          )
-          Icon(
-            modifier = Modifier.align(Alignment.CenterVertically),
-            imageVector =
-              if (isBlocklistShown) {
-                Icons.Rounded.KeyboardArrowUp
-              } else {
-                Icons.Rounded.KeyboardArrowDown
-              },
-            contentDescription =
-              if (isBlocklistShown) {
-                stringResource(R.string.collapse_icon)
-              } else {
-                stringResource(R.string.expand_icon)
-              },
+            modifier = Modifier.padding(16.dp),
           )
         }
-      }
-      if (isBlocklistShown) {
-        if (blockedClients.isEmpty()) {
+        if (tetheredClients.isEmpty()) {
           item {
             Box(
               modifier = Modifier.fillMaxWidth(),
               contentAlignment = Alignment.Center,
             ) {
-              Text(text = stringResource(R.string.blocklist_none_blocked))
+              Text(text = stringResource(R.string.no_connected_devices))
             }
           }
         }
-        items(blockedClients) { d ->
+        items(tetheredClients) { client ->
           Row(
-            modifier = Modifier.padding(16.dp),
+            modifier =
+              Modifier.fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 4.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically,
           ) {
-            Column(modifier = Modifier.weight(1f)) {
-              Text(text = d.hostname ?: noClientHostnameText)
-              Text(text = d.macAddress.toString())
+            Column(modifier = Modifier.padding(horizontal = 8.dp)) {
+              Text(
+                text =
+                  client.hostname ?: stringResource(R.string.no_client_hostname)
+              )
+
+              client.address?.address?.hostAddress.let { ip ->
+                Row(
+                  horizontalArrangement = Arrangement.spacedBy(8.dp),
+                  verticalAlignment = Alignment.CenterVertically,
+                  modifier =
+                    Modifier.clickable {
+                      ip ?: return@clickable
+                      clipboardManager.setText(AnnotatedString(ip))
+                    },
+                ) {
+                  Text(text = ip ?: stringResource(R.string.ip_not_allocated))
+
+                  if (ip != null) {
+                    Icon(
+                      imageVector = Icons.Rounded.ContentCopy,
+                      contentDescription = stringResource(R.string.copy_button),
+                      tint = MaterialTheme.colorScheme.primary,
+                      modifier =
+                        Modifier.size(
+                          with(density) {
+                            LocalTextStyle.current.fontSize.toDp()
+                          }
+                        ),
+                    )
+                  }
+                }
+              }
             }
             Button(
               onClick = {
-                vm.unblockDevice(d)
-                scope.launch {
-                  snackbarHostState.showSnackbar(
-                    message = blocklistUnblockedText,
-                    duration = SnackbarDuration.Short,
-                    withDismissAction = true,
-                  )
-                }
+                vm.blockDevice(
+                  device =
+                    ACLDevice(
+                      hostname = client.hostname,
+                      macAddress = client.macAddress,
+                    )
+                )
               }
             ) {
-              Text(text = stringResource(R.string.unblock_button))
+              Text(text = stringResource(R.string.block_button))
+            }
+          }
+        }
+        item {
+          Row(
+            modifier =
+              Modifier.fillMaxWidth()
+                .clickable { isBlocklistShown = !isBlocklistShown }
+                .padding(16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+          ) {
+            Text(
+              text = stringResource(R.string.blocklist),
+              color = MaterialTheme.colorScheme.onSurfaceVariant,
+              style = MaterialTheme.typography.titleLarge,
+            )
+            Icon(
+              modifier = Modifier.align(Alignment.CenterVertically),
+              imageVector =
+                if (isBlocklistShown) {
+                  Icons.Rounded.KeyboardArrowUp
+                } else {
+                  Icons.Rounded.KeyboardArrowDown
+                },
+              contentDescription =
+                if (isBlocklistShown) {
+                  stringResource(R.string.collapse_icon)
+                } else {
+                  stringResource(R.string.expand_icon)
+                },
+            )
+          }
+        }
+        if (isBlocklistShown) {
+          if (blockedClients.isEmpty()) {
+            item {
+              Box(
+                modifier = Modifier.fillMaxWidth(),
+                contentAlignment = Alignment.Center,
+              ) {
+                Text(text = stringResource(R.string.blocklist_none_blocked))
+              }
+            }
+          }
+          items(blockedClients) { d ->
+            Row(
+              modifier = Modifier.padding(16.dp),
+              verticalAlignment = Alignment.CenterVertically,
+            ) {
+              Column(modifier = Modifier.weight(1f)) {
+                Text(text = d.hostname ?: noClientHostnameText)
+                Text(text = d.macAddress.toString())
+              }
+              Button(
+                onClick = {
+                  vm.unblockDevice(d)
+                  scope.launch {
+                    snackbarHostState.showSnackbar(
+                      message = blocklistUnblockedText,
+                      duration = SnackbarDuration.Short,
+                      withDismissAction = true,
+                    )
+                  }
+                }
+              ) {
+                Text(text = stringResource(R.string.unblock_button))
+              }
             }
           }
         }
