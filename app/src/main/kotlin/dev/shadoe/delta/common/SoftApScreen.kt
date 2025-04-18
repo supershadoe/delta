5e9088767a6fd78103902a97472a599c970bc933
supershadoe <shadoe@shadoe.dev>
2025-04-18 23:21:30 +0530

    Constrain UI by width
    It makes no sense to render edge to edge on very large devices, harder to find UI elements.
    
    It's also harder to design for random sizes without having access to such devices.

--- app/src/main/kotlin/dev/shadoe/delta/common/SoftApScreen.kt
+++ app/src/main/kotlin/dev/shadoe/delta/common/SoftApScreen.kt
@@ -1,5 +1,8 @@
 package dev.shadoe.delta.common
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
@@ -8,7 +11,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import dev.shadoe.delta.control.ControlViewModel
 import dev.shadoe.delta.control.NewControlScreen
@@ -21,34 +27,38 @@ fun SoftApScreen() {
   var currentDestination by rememberSaveable {
     mutableStateOf(SoftApScreenDestinations.CONTROL)
   }
-  NavigationSuiteScaffold(
-    navigationSuiteItems = {
-      SoftApScreenDestinations.entries.forEach {
-        item(
-          label = { Text(text = stringResource(it.label)) },
-          icon = {
-            Icon(
-              imageVector = it.icon,
-              contentDescription = stringResource(it.label),
-            )
-          },
-          selected = it == currentDestination,
-          onClick = { currentDestination = it },
-        )
-      }
-    }
-  ) {
-    when (currentDestination) {
-      SoftApScreenDestinations.CONTROL -> {
-        val vm = hiltViewModel<ControlViewModel>()
-        NewControlScreen(
-          onNavigateToDebug = { navController.navigate(Routes.DebugScreen) },
-          vm = vm,
-        )
-      }
-      SoftApScreenDestinations.SETTINGS -> {
-        val vm = hiltViewModel<SettingsViewModel>()
-        SettingsScreen(vm = vm)
+  Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
+    NavigationSuiteScaffold(
+      navigationSuiteItems = {
+        SoftApScreenDestinations.entries.forEach {
+          item(
+            label = { Text(text = stringResource(it.label)) },
+            icon = {
+              Icon(
+                imageVector = it.icon,
+                contentDescription = stringResource(it.label),
+              )
+            },
+            selected = it == currentDestination,
+            onClick = { currentDestination = it },
+          )
+        }
+      },
+      modifier = Modifier.widthIn(max = 2160.dp),
+    ) {
+      when (currentDestination) {
+        SoftApScreenDestinations.CONTROL -> {
+          val vm = hiltViewModel<ControlViewModel>()
+          NewControlScreen(
+            onNavigateToDebug = { navController.navigate(Routes.DebugScreen) },
+            vm = vm,
+          )
+        }
+
+        SoftApScreenDestinations.SETTINGS -> {
+          val vm = hiltViewModel<SettingsViewModel>()
+          SettingsScreen(vm = vm)
+        }
       }
     }
   }
