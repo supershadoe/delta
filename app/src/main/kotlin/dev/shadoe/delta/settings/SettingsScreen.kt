f7df2dc43d2ce9a82f1a5e47df56475832d504ad
supershadoe <shadoe@shadoe.dev>
2025-04-23 08:58:37 +0000

    Move scaling concerns to just SoftApScreen
    Parent sets the size, not the screens themselves

--- app/src/main/kotlin/dev/shadoe/delta/settings/SettingsScreen.kt
+++ app/src/main/kotlin/dev/shadoe/delta/settings/SettingsScreen.kt
@@ -111,235 +111,232 @@ fun SettingsScreen(
   val appBarScrollBehavior =
     TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
 
-  Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
-    Scaffold(
-      topBar = {
-        @OptIn(ExperimentalMaterial3Api::class)
-        LargeTopAppBar(
-          title = { Text(text = stringResource(R.string.settings)) },
-          actions = {
-            val context = LocalContext.current
-            IconButton(onClick = { openSystemSettings(context, isBigScreen) }) {
-              Icon(
-                imageVector = Icons.AutoMirrored.Rounded.OpenInNew,
-                contentDescription =
-                  stringResource(R.string.open_system_settings),
-              )
-            }
-          },
-          scrollBehavior = appBarScrollBehavior,
-        )
-      },
-      modifier =
+  Scaffold(
+    topBar = {
+      @OptIn(ExperimentalMaterial3Api::class)
+      LargeTopAppBar(
+        title = { Text(text = stringResource(R.string.settings)) },
+        actions = {
+          val context = LocalContext.current
+          IconButton(onClick = { openSystemSettings(context, isBigScreen) }) {
+            Icon(
+              imageVector = Icons.AutoMirrored.Rounded.OpenInNew,
+              contentDescription = stringResource(R.string.open_system_settings),
+            )
+          }
+        },
+        scrollBehavior = appBarScrollBehavior,
+      )
+    },
+    modifier =
+      modifier.then(
         Modifier.widthIn(max = 600.dp)
           .nestedScroll(
             @OptIn(ExperimentalMaterial3Api::class)
             appBarScrollBehavior.nestedScrollConnection
-          ),
-      snackbarHost = { SnackbarHost(snackbarHostState) },
-    ) { scaffoldPadding ->
-      LazyColumn(
-        modifier =
-          Modifier.fillMaxSize()
-            .padding(scaffoldPadding)
-            .padding(horizontal = 16.dp)
-            .pointerInput(Unit) {
-              detectTapGestures(onTap = { focusManager.clearFocus() })
-            }
-            .then(modifier)
-      ) {
-        if (Build.MANUFACTURER == "samsung") {
-          item {
-            Box(
-              modifier =
-                Modifier.background(
-                  color = MaterialTheme.colorScheme.tertiaryContainer,
-                  shape = RoundedCornerShape(12.dp),
-                )
+          )
+      ),
+    snackbarHost = { SnackbarHost(snackbarHostState) },
+  ) { scaffoldPadding ->
+    LazyColumn(
+      modifier =
+        Modifier.fillMaxSize()
+          .padding(scaffoldPadding)
+          .padding(horizontal = 16.dp)
+          .pointerInput(Unit) {
+            detectTapGestures(onTap = { focusManager.clearFocus() })
+          }
+    ) {
+      if (Build.MANUFACTURER == "samsung") {
+        item {
+          Box(
+            modifier =
+              Modifier.background(
+                color = MaterialTheme.colorScheme.tertiaryContainer,
+                shape = RoundedCornerShape(12.dp),
+              )
+          ) {
+            Row(
+              modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp),
+              verticalAlignment = Alignment.CenterVertically,
             ) {
-              Row(
-                modifier =
-                  Modifier.padding(horizontal = 16.dp, vertical = 8.dp),
-                verticalAlignment = Alignment.CenterVertically,
-              ) {
-                Icon(
-                  imageVector = Icons.Outlined.Warning,
-                  contentDescription = stringResource(R.string.warning_icon),
+              Icon(
+                imageVector = Icons.Outlined.Warning,
+                contentDescription = stringResource(R.string.warning_icon),
+              )
+              Column(modifier = Modifier.padding(start = 16.dp)) {
+                val context = LocalContext.current
+                Text(
+                  text = stringResource(R.string.settings_warn_samsung),
+                  color = MaterialTheme.colorScheme.onTertiaryContainer,
                 )
-                Column(modifier = Modifier.padding(start = 16.dp)) {
-                  val context = LocalContext.current
-                  Text(
-                    text = stringResource(R.string.settings_warn_samsung),
-                    color = MaterialTheme.colorScheme.onTertiaryContainer,
+                Button(
+                  onClick = { openSystemSettings(context, isBigScreen) },
+                  modifier = Modifier.padding(top = 8.dp),
+                ) {
+                  Icon(
+                    imageVector = Icons.AutoMirrored.Rounded.OpenInNew,
+                    contentDescription = stringResource(R.string.open_icon),
                   )
-                  Button(
-                    onClick = { openSystemSettings(context, isBigScreen) },
-                    modifier = Modifier.padding(top = 8.dp),
-                  ) {
-                    Icon(
-                      imageVector = Icons.AutoMirrored.Rounded.OpenInNew,
-                      contentDescription = stringResource(R.string.open_icon),
-                    )
-                    Text(
-                      text = stringResource(R.string.open_system_settings),
-                      modifier = Modifier.padding(start = 8.dp),
-                    )
-                  }
+                  Text(
+                    text = stringResource(R.string.open_system_settings),
+                    modifier = Modifier.padding(start = 8.dp),
+                  )
                 }
               }
             }
           }
         }
+      }
+      item {
+        Text(
+          text = stringResource(R.string.settings_desc),
+          modifier = Modifier.padding(8.dp),
+        )
+      }
+      item {
+        SsidField(
+          ssid = config.ssid ?: "",
+          onSsidChange = { vm.updateSsid(it) },
+          currentResult = results.ssidResult,
+        )
+      }
+      item {
+        SecurityTypeField(
+          securityType = config.securityType,
+          supportedSecurityTypes = status.capabilities.supportedSecurityTypes,
+          onSecurityTypeChange = { vm.updateSecurityType(it) },
+        )
+      }
+      if (config.securityType != SoftApSecurityType.SECURITY_TYPE_OPEN) {
         item {
+          PassphraseField(
+            passphrase = config.passphrase,
+            onPassphraseChange = { vm.updatePassphrase(it) },
+            currentResult = results.passphraseResult,
+          )
+        }
+      }
+      item {
+        AutoShutdownField(
+          isAutoShutdownEnabled = config.isAutoShutdownEnabled,
+          onAutoShutdownChange = { vm.updateAutoShutdown(it) },
+        )
+      }
+      item {
+        FrequencyBandField(
+          frequencyBand = config.speedType,
+          supportedBands = status.capabilities.supportedFrequencyBands,
+          onBandChange = { vm.updateSpeedType(it) },
+        )
+      }
+
+      item {
+        Row(
+          modifier =
+            Modifier.fillMaxWidth().clickable {
+              isAdvancedSettingsEnabled = !isAdvancedSettingsEnabled
+            },
+          horizontalArrangement = Arrangement.SpaceBetween,
+        ) {
           Text(
-            text = stringResource(R.string.settings_desc),
+            text = stringResource(R.string.advanced_settings_label),
             modifier = Modifier.padding(8.dp),
           )
-        }
-        item {
-          SsidField(
-            ssid = config.ssid ?: "",
-            onSsidChange = { vm.updateSsid(it) },
-            currentResult = results.ssidResult,
-          )
-        }
-        item {
-          SecurityTypeField(
-            securityType = config.securityType,
-            supportedSecurityTypes = status.capabilities.supportedSecurityTypes,
-            onSecurityTypeChange = { vm.updateSecurityType(it) },
-          )
-        }
-        if (config.securityType != SoftApSecurityType.SECURITY_TYPE_OPEN) {
-          item {
-            PassphraseField(
-              passphrase = config.passphrase,
-              onPassphraseChange = { vm.updatePassphrase(it) },
-              currentResult = results.passphraseResult,
-            )
-          }
-        }
-        item {
-          AutoShutdownField(
-            isAutoShutdownEnabled = config.isAutoShutdownEnabled,
-            onAutoShutdownChange = { vm.updateAutoShutdown(it) },
-          )
-        }
-        item {
-          FrequencyBandField(
-            frequencyBand = config.speedType,
-            supportedBands = status.capabilities.supportedFrequencyBands,
-            onBandChange = { vm.updateSpeedType(it) },
-          )
-        }
-
-        item {
-          Row(
-            modifier =
-              Modifier.fillMaxWidth().clickable {
-                isAdvancedSettingsEnabled = !isAdvancedSettingsEnabled
+          Icon(
+            modifier = Modifier.align(Alignment.CenterVertically),
+            imageVector =
+              if (isAdvancedSettingsEnabled) {
+                Icons.Rounded.KeyboardArrowUp
+              } else {
+                Icons.Rounded.KeyboardArrowDown
               },
-            horizontalArrangement = Arrangement.SpaceBetween,
-          ) {
-            Text(
-              text = stringResource(R.string.advanced_settings_label),
-              modifier = Modifier.padding(8.dp),
-            )
-            Icon(
-              modifier = Modifier.align(Alignment.CenterVertically),
-              imageVector =
-                if (isAdvancedSettingsEnabled) {
-                  Icons.Rounded.KeyboardArrowUp
-                } else {
-                  Icons.Rounded.KeyboardArrowDown
-                },
-              contentDescription =
-                if (isAdvancedSettingsEnabled) {
-                  stringResource(R.string.collapse_icon)
-                } else {
-                  stringResource(R.string.expand_icon)
-                },
-            )
-          }
+            contentDescription =
+              if (isAdvancedSettingsEnabled) {
+                stringResource(R.string.collapse_icon)
+              } else {
+                stringResource(R.string.expand_icon)
+              },
+          )
         }
-        if (isAdvancedSettingsEnabled) {
-          item {
-            HiddenHotspotField(
-              isHiddenHotspotEnabled = config.isHidden,
-              onHiddenHotspotChange = { vm.updateIsHidden(it) },
-            )
-          }
-          item {
-            MaxClientLimitField(
-              maxClient = status.capabilities.maxSupportedClients,
-              onMaxClientChange = { vm.updateMaxClientLimit(it.toInt()) },
-              allowedLimit = config.maxClientLimit,
-            )
-          }
-          item {
-            MacRandomizationField(
-              macRandomizationSetting = config.macRandomizationSetting,
-              onSettingChange = { vm.updateMacRandomizationSetting(it) },
-            )
-          }
-          item {
-            AutoShutDownTimeOutField(
-              autoShutDownTimeOut =
-                config.autoShutdownTimeout.takeIf {
-                  it in SoftApAutoShutdownTimeout.supportedShutdownTimeouts
-                } ?: SoftApAutoShutdownTimeout.DEFAULT,
-              supportedAutoShutdownType =
-                SoftApAutoShutdownTimeout.supportedShutdownTimeouts,
-              onAutoShutdownChange = { vm.updateAutoShutdownTimeout(it) },
-            )
-          }
-          item {
-            PresetField(
-              onShowPresets = { isPresetListShown = true },
-              onSaveConfig = { shouldSavePreset = true },
-            )
-          }
-          if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            item { SoftApTileField() }
-          }
+      }
+      if (isAdvancedSettingsEnabled) {
+        item {
+          HiddenHotspotField(
+            isHiddenHotspotEnabled = config.isHidden,
+            onHiddenHotspotChange = { vm.updateIsHidden(it) },
+          )
         }
         item {
-          Button(
-            onClick = onClick@{
-                if (
-                  config.passphrase.isEmpty() &&
-                    config.securityType != SoftApSecurityType.SECURITY_TYPE_OPEN
-                ) {
-                  scope.launch {
-                    snackbarHostState.showSnackbar(
-                      message = passphraseEmptyWarningText,
-                      withDismissAction = true,
-                      duration = SnackbarDuration.Short,
-                    )
-                  }
-                  return@onClick
+          MaxClientLimitField(
+            maxClient = status.capabilities.maxSupportedClients,
+            onMaxClientChange = { vm.updateMaxClientLimit(it.toInt()) },
+            allowedLimit = config.maxClientLimit,
+          )
+        }
+        item {
+          MacRandomizationField(
+            macRandomizationSetting = config.macRandomizationSetting,
+            onSettingChange = { vm.updateMacRandomizationSetting(it) },
+          )
+        }
+        item {
+          AutoShutDownTimeOutField(
+            autoShutDownTimeOut =
+              config.autoShutdownTimeout.takeIf {
+                it in SoftApAutoShutdownTimeout.supportedShutdownTimeouts
+              } ?: SoftApAutoShutdownTimeout.DEFAULT,
+            supportedAutoShutdownType =
+              SoftApAutoShutdownTimeout.supportedShutdownTimeouts,
+            onAutoShutdownChange = { vm.updateAutoShutdownTimeout(it) },
+          )
+        }
+        item {
+          PresetField(
+            onShowPresets = { isPresetListShown = true },
+            onSaveConfig = { shouldSavePreset = true },
+          )
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+          item { SoftApTileField() }
+        }
+      }
+      item {
+        Button(
+          onClick = onClick@{
+              if (
+                config.passphrase.isEmpty() &&
+                  config.securityType != SoftApSecurityType.SECURITY_TYPE_OPEN
+              ) {
+                scope.launch {
+                  snackbarHostState.showSnackbar(
+                    message = passphraseEmptyWarningText,
+                    withDismissAction = true,
+                    duration = SnackbarDuration.Short,
+                  )
                 }
-                if (!vm.commit()) {
-                  scope.launch {
-                    snackbarHostState.showSnackbar(
-                      message = failedToSaveText,
-                      withDismissAction = true,
-                      duration = SnackbarDuration.Short,
-                    )
-                  }
-                } else {
-                  scope.launch {
-                    snackbarHostState.showSnackbar(
-                      message = savedText,
-                      withDismissAction = true,
-                      duration = SnackbarDuration.Short,
-                    )
-                  }
+                return@onClick
+              }
+              if (!vm.commit()) {
+                scope.launch {
+                  snackbarHostState.showSnackbar(
+                    message = failedToSaveText,
+                    withDismissAction = true,
+                    duration = SnackbarDuration.Short,
+                  )
+                }
+              } else {
+                scope.launch {
+                  snackbarHostState.showSnackbar(
+                    message = savedText,
+                    withDismissAction = true,
+                    duration = SnackbarDuration.Short,
+                  )
                 }
               }
-          ) {
-            Text(text = stringResource(R.string.save_button))
-          }
+            }
+        ) {
+          Text(text = stringResource(R.string.save_button))
         }
       }
     }
