a592797edea252d7f71003583729bd377ebedf2b
supershadoe <shadoe@shadoe.dev>
2025-04-25 23:14:22 +0530

    Use TabRow instead of NavigationSuiteScaffold
    It makes more sense to use a tab row for the only two actions in the control screen.

--- app/src/main/kotlin/dev/shadoe/delta/common/SoftApScreen.kt
+++ app/src/main/kotlin/dev/shadoe/delta/common/SoftApScreen.kt
@@ -1,15 +1,28 @@
 package dev.shadoe.delta.common
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
-import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -18,54 +31,86 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import dev.shadoe.delta.control.ControlScreen
 import dev.shadoe.delta.control.ControlViewModel
+import dev.shadoe.delta.control.components.AppBarWithDebugAction
 import dev.shadoe.delta.settings.SettingsScreen
 import dev.shadoe.delta.settings.SettingsViewModel
+import kotlinx.coroutines.launch
 
 @Composable
 fun SoftApScreen() {
   val navController = LocalNavController.current
-  var currentDestination by rememberSaveable {
-    mutableStateOf(SoftApScreenDestinations.CONTROL)
-  }
-  Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
-    NavigationSuiteScaffold(
-      navigationSuiteItems = {
-        SoftApScreenDestinations.entries.forEach {
-          item(
-            label = { Text(text = stringResource(it.label)) },
-            icon = {
-              Icon(
-                imageVector = it.icon,
-                contentDescription = stringResource(it.label),
-              )
-            },
-            selected = it == currentDestination,
-            onClick = { currentDestination = it },
-          )
-        }
-      },
+  val scope = rememberCoroutineScope()
+  val snackbarHostState = remember { SnackbarHostState() }
+  val pagerState =
+    rememberPagerState(pageCount = { SoftApScreenDestinations.entries.size })
+
+  Box(
+    modifier =
+      Modifier.background(MaterialTheme.colorScheme.background).fillMaxWidth(),
+    contentAlignment = Alignment.Center,
+  ) {
+    Scaffold(
       modifier = Modifier.widthIn(max = 2160.dp),
-    ) {
-      val modifier = Modifier.widthIn(max = 600.dp)
-      Box(
-        modifier = Modifier.fillMaxWidth(),
-        contentAlignment = Alignment.Center,
-      ) {
-        when (currentDestination) {
-          SoftApScreenDestinations.CONTROL -> {
-            val vm = hiltViewModel<ControlViewModel>()
-            ControlScreen(
-              onNavigateToDebug = {
-                navController.navigate(Routes.DebugScreen)
-              },
-              modifier,
-              vm,
+      topBar = {
+        @OptIn(ExperimentalMaterial3Api::class)
+        CenterAlignedTopAppBar(
+          title = {
+            AppBarWithDebugAction(
+              onNavigateToDebug = { navController.navigate(Routes.DebugScreen) }
             )
           }
+        )
+      },
+      snackbarHost = { SnackbarHost(snackbarHostState) },
+    ) { scaffoldPadding ->
+      Column(modifier = Modifier.padding(scaffoldPadding).fillMaxWidth()) {
+        TabRow(selectedTabIndex = pagerState.currentPage) {
+          SoftApScreenDestinations.entries.forEach {
+            Tab(
+              selected = pagerState.currentPage == it.ordinal,
+              onClick = {
+                scope.launch { pagerState.animateScrollToPage(it.ordinal) }
+              },
+              text = { Text(stringResource(it.label)) },
+              icon = {
+                Icon(
+                  imageVector = it.icon,
+                  contentDescription = stringResource(it.contentDescription),
+                )
+              },
+            )
+          }
+        }
 
-          SoftApScreenDestinations.SETTINGS -> {
-            val vm = hiltViewModel<SettingsViewModel>()
-            SettingsScreen(modifier, vm)
+        HorizontalPager(
+          state = pagerState,
+          modifier = Modifier.fillMaxWidth().weight(1f),
+        ) {
+          Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.TopCenter,
+          ) {
+            when (SoftApScreenDestinations.entries[it]) {
+              SoftApScreenDestinations.CONTROL -> {
+                val vm = hiltViewModel<ControlViewModel>()
+                ControlScreen(
+                  onShowSnackbar = { visuals ->
+                    scope.launch { snackbarHostState.showSnackbar(visuals) }
+                  },
+                  vm = vm,
+                )
+              }
+
+              SoftApScreenDestinations.SETTINGS -> {
+                val vm = hiltViewModel<SettingsViewModel>()
+                SettingsScreen(
+                  onShowSnackbar = { visuals ->
+                    scope.launch { snackbarHostState.showSnackbar(visuals) }
+                  },
+                  vm = vm,
+                )
+              }
+            }
           }
         }
       }
--- app/src/main/kotlin/dev/shadoe/delta/settings/SettingsScreen.kt
+++ app/src/main/kotlin/dev/shadoe/delta/settings/SettingsScreen.kt
@@ -13,7 +13,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -24,26 +23,19 @@ import androidx.compose.material.icons.rounded.KeyboardArrowUp
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.LargeTopAppBar
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarDuration
-import androidx.compose.material3.SnackbarHost
-import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarVisuals
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
@@ -67,7 +59,6 @@ import dev.shadoe.delta.settings.components.PresetSheet
 import dev.shadoe.delta.settings.components.SecurityTypeField
 import dev.shadoe.delta.settings.components.SoftApTileField
 import dev.shadoe.delta.settings.components.SsidField
-import kotlinx.coroutines.launch
 
 private fun openSystemSettings(context: Context, isBigScreen: Boolean = false) {
   context.startActivity(
@@ -84,260 +75,229 @@ private fun openSystemSettings(context: Context, isBigScreen: Boolean = false) {
 @Composable
 fun SettingsScreen(
   modifier: Modifier = Modifier,
+  onShowSnackbar: (SnackbarVisuals) -> Unit = {},
   vm: SettingsViewModel = viewModel(),
 ) {
   val focusManager = LocalFocusManager.current
   val isBigScreen = LocalConfiguration.current.screenWidthDp >= 700
   @OptIn(ExperimentalMaterial3Api::class)
   val sheetState = rememberModalBottomSheetState()
-  val scope = rememberCoroutineScope()
-  val snackbarHostState = remember { SnackbarHostState() }
 
   val config by vm.config.collectAsState()
   val status by vm.status.collectAsState()
   val results by vm.results.collectAsState()
   val presets by vm.presets.collectAsState(listOf())
 
-  val passphraseEmptyWarningText =
-    stringResource(R.string.passphrase_empty_warning)
-  val failedToSaveText = stringResource(R.string.save_changes_failed_warning)
-  val savedText = stringResource(R.string.save_changes_succeeded)
+  val passphraseEmptyWarningSnackbar =
+    object : SnackbarVisuals {
+      override val message = stringResource(R.string.passphrase_empty_warning)
+      override val withDismissAction = true
+      override val actionLabel = null
+      override val duration = SnackbarDuration.Short
+    }
+
+  val failedToSaveSnackbar =
+    object : SnackbarVisuals {
+      override val message =
+        stringResource(R.string.save_changes_failed_warning)
+      override val withDismissAction = true
+      override val actionLabel = null
+      override val duration = SnackbarDuration.Short
+    }
+
+  val savedSnackbar =
+    object : SnackbarVisuals {
+      override val message = stringResource(R.string.save_changes_succeeded)
+      override val withDismissAction = true
+      override val actionLabel = null
+      override val duration = SnackbarDuration.Short
+    }
 
   var isAdvancedSettingsEnabled by remember { mutableStateOf(false) }
   var shouldSavePreset by remember { mutableStateOf(false) }
   var isPresetListShown by remember { mutableStateOf(false) }
 
-  @OptIn(ExperimentalMaterial3Api::class)
-  val appBarScrollBehavior =
-    TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
-
-  Scaffold(
-    topBar = {
-      @OptIn(ExperimentalMaterial3Api::class)
-      LargeTopAppBar(
-        title = { Text(text = stringResource(R.string.settings)) },
-        actions = {
-          val context = LocalContext.current
-          IconButton(onClick = { openSystemSettings(context, isBigScreen) }) {
-            Icon(
-              imageVector = Icons.AutoMirrored.Rounded.OpenInNew,
-              contentDescription = stringResource(R.string.open_system_settings),
-            )
-          }
-        },
-        scrollBehavior = appBarScrollBehavior,
-      )
-    },
+  LazyColumn(
     modifier =
       modifier.then(
-        Modifier.widthIn(max = 600.dp)
-          .nestedScroll(
-            @OptIn(ExperimentalMaterial3Api::class)
-            appBarScrollBehavior.nestedScrollConnection
-          )
-      ),
-    snackbarHost = { SnackbarHost(snackbarHostState) },
-  ) { scaffoldPadding ->
-    LazyColumn(
-      modifier =
-        Modifier.fillMaxSize()
-          .padding(scaffoldPadding)
-          .padding(horizontal = 16.dp)
-          .pointerInput(Unit) {
+        other =
+          Modifier.fillMaxSize().padding(horizontal = 16.dp).pointerInput(
+            Unit
+          ) {
             detectTapGestures(onTap = { focusManager.clearFocus() })
           }
-    ) {
-      if (Build.MANUFACTURER == "samsung") {
-        item {
-          Box(
-            modifier =
-              Modifier.background(
-                color = MaterialTheme.colorScheme.tertiaryContainer,
-                shape = RoundedCornerShape(12.dp),
-              )
+      )
+  ) {
+    if (Build.MANUFACTURER == "samsung") {
+      item {
+        Box(
+          modifier =
+            Modifier.background(
+              color = MaterialTheme.colorScheme.tertiaryContainer,
+              shape = RoundedCornerShape(12.dp),
+            )
+        ) {
+          Row(
+            modifier = Modifier.padding(horizontal = 16.dp, vertical = 16.dp),
+            verticalAlignment = Alignment.CenterVertically,
           ) {
-            Row(
-              modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp),
-              verticalAlignment = Alignment.CenterVertically,
-            ) {
-              Icon(
-                imageVector = Icons.Outlined.Warning,
-                contentDescription = stringResource(R.string.warning_icon),
+            Icon(
+              imageVector = Icons.Outlined.Warning,
+              contentDescription = stringResource(R.string.warning_icon),
+            )
+            Column(modifier = Modifier.padding(start = 16.dp)) {
+              val context = LocalContext.current
+              Text(
+                text = stringResource(R.string.settings_warn_samsung),
+                color = MaterialTheme.colorScheme.onTertiaryContainer,
               )
-              Column(modifier = Modifier.padding(start = 16.dp)) {
-                val context = LocalContext.current
-                Text(
-                  text = stringResource(R.string.settings_warn_samsung),
-                  color = MaterialTheme.colorScheme.onTertiaryContainer,
+              Button(
+                onClick = { openSystemSettings(context, isBigScreen) },
+                modifier = Modifier.padding(top = 8.dp),
+              ) {
+                Icon(
+                  imageVector = Icons.AutoMirrored.Rounded.OpenInNew,
+                  contentDescription = stringResource(R.string.open_icon),
                 )
-                Button(
-                  onClick = { openSystemSettings(context, isBigScreen) },
-                  modifier = Modifier.padding(top = 8.dp),
-                ) {
-                  Icon(
-                    imageVector = Icons.AutoMirrored.Rounded.OpenInNew,
-                    contentDescription = stringResource(R.string.open_icon),
-                  )
-                  Text(
-                    text = stringResource(R.string.open_system_settings),
-                    modifier = Modifier.padding(start = 8.dp),
-                  )
-                }
+                Text(
+                  text = stringResource(R.string.open_system_settings),
+                  modifier = Modifier.padding(start = 8.dp),
+                )
               }
             }
           }
         }
       }
+    }
+    item {
+      Text(
+        text = stringResource(R.string.settings_desc),
+        modifier = Modifier.padding(16.dp),
+      )
+    }
+    item {
+      SsidField(
+        ssid = config.ssid ?: "",
+        onSsidChange = { vm.updateSsid(it) },
+        currentResult = results.ssidResult,
+      )
+    }
+    item {
+      SecurityTypeField(
+        securityType = config.securityType,
+        supportedSecurityTypes = status.capabilities.supportedSecurityTypes,
+        onSecurityTypeChange = { vm.updateSecurityType(it) },
+      )
+    }
+    if (config.securityType != SoftApSecurityType.SECURITY_TYPE_OPEN) {
       item {
+        PassphraseField(
+          passphrase = config.passphrase,
+          onPassphraseChange = { vm.updatePassphrase(it) },
+          currentResult = results.passphraseResult,
+        )
+      }
+    }
+    item {
+      AutoShutdownField(
+        isAutoShutdownEnabled = config.isAutoShutdownEnabled,
+        onAutoShutdownChange = { vm.updateAutoShutdown(it) },
+      )
+    }
+    item {
+      FrequencyBandField(
+        frequencyBand = config.speedType,
+        supportedBands = status.capabilities.supportedFrequencyBands,
+        onBandChange = { vm.updateSpeedType(it) },
+      )
+    }
+
+    item {
+      Row(
+        modifier =
+          Modifier.fillMaxWidth().clickable {
+            isAdvancedSettingsEnabled = !isAdvancedSettingsEnabled
+          },
+        horizontalArrangement = Arrangement.SpaceBetween,
+      ) {
         Text(
-          text = stringResource(R.string.settings_desc),
+          text = stringResource(R.string.advanced_settings_label),
           modifier = Modifier.padding(8.dp),
         )
-      }
-      item {
-        SsidField(
-          ssid = config.ssid ?: "",
-          onSsidChange = { vm.updateSsid(it) },
-          currentResult = results.ssidResult,
-        )
-      }
-      item {
-        SecurityTypeField(
-          securityType = config.securityType,
-          supportedSecurityTypes = status.capabilities.supportedSecurityTypes,
-          onSecurityTypeChange = { vm.updateSecurityType(it) },
-        )
-      }
-      if (config.securityType != SoftApSecurityType.SECURITY_TYPE_OPEN) {
-        item {
-          PassphraseField(
-            passphrase = config.passphrase,
-            onPassphraseChange = { vm.updatePassphrase(it) },
-            currentResult = results.passphraseResult,
-          )
-        }
-      }
-      item {
-        AutoShutdownField(
-          isAutoShutdownEnabled = config.isAutoShutdownEnabled,
-          onAutoShutdownChange = { vm.updateAutoShutdown(it) },
-        )
-      }
-      item {
-        FrequencyBandField(
-          frequencyBand = config.speedType,
-          supportedBands = status.capabilities.supportedFrequencyBands,
-          onBandChange = { vm.updateSpeedType(it) },
-        )
-      }
-
-      item {
-        Row(
-          modifier =
-            Modifier.fillMaxWidth().clickable {
-              isAdvancedSettingsEnabled = !isAdvancedSettingsEnabled
+        Icon(
+          modifier = Modifier.align(Alignment.CenterVertically),
+          imageVector =
+            if (isAdvancedSettingsEnabled) {
+              Icons.Rounded.KeyboardArrowUp
+            } else {
+              Icons.Rounded.KeyboardArrowDown
             },
-          horizontalArrangement = Arrangement.SpaceBetween,
-        ) {
-          Text(
-            text = stringResource(R.string.advanced_settings_label),
-            modifier = Modifier.padding(8.dp),
-          )
-          Icon(
-            modifier = Modifier.align(Alignment.CenterVertically),
-            imageVector =
-              if (isAdvancedSettingsEnabled) {
-                Icons.Rounded.KeyboardArrowUp
-              } else {
-                Icons.Rounded.KeyboardArrowDown
-              },
-            contentDescription =
-              if (isAdvancedSettingsEnabled) {
-                stringResource(R.string.collapse_icon)
-              } else {
-                stringResource(R.string.expand_icon)
-              },
-          )
-        }
+          contentDescription =
+            if (isAdvancedSettingsEnabled) {
+              stringResource(R.string.collapse_icon)
+            } else {
+              stringResource(R.string.expand_icon)
+            },
+        )
       }
-      if (isAdvancedSettingsEnabled) {
-        item {
-          HiddenHotspotField(
-            isHiddenHotspotEnabled = config.isHidden,
-            onHiddenHotspotChange = { vm.updateIsHidden(it) },
-          )
-        }
-        item {
-          MaxClientLimitField(
-            maxClient = status.capabilities.maxSupportedClients,
-            onMaxClientChange = { vm.updateMaxClientLimit(it.toInt()) },
-            allowedLimit = config.maxClientLimit,
-          )
-        }
-        item {
-          MacRandomizationField(
-            macRandomizationSetting = config.macRandomizationSetting,
-            onSettingChange = { vm.updateMacRandomizationSetting(it) },
-          )
-        }
-        item {
-          AutoShutDownTimeOutField(
-            autoShutDownTimeOut =
-              config.autoShutdownTimeout.takeIf {
-                it in SoftApAutoShutdownTimeout.supportedShutdownTimeouts
-              } ?: SoftApAutoShutdownTimeout.DEFAULT,
-            supportedAutoShutdownType =
-              SoftApAutoShutdownTimeout.supportedShutdownTimeouts,
-            onAutoShutdownChange = { vm.updateAutoShutdownTimeout(it) },
-          )
-        }
-        item {
-          PresetField(
-            onShowPresets = { isPresetListShown = true },
-            onSaveConfig = { shouldSavePreset = true },
-          )
-        }
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-          item { SoftApTileField() }
-        }
+    }
+    if (isAdvancedSettingsEnabled) {
+      item {
+        HiddenHotspotField(
+          isHiddenHotspotEnabled = config.isHidden,
+          onHiddenHotspotChange = { vm.updateIsHidden(it) },
+        )
       }
       item {
-        Button(
-          onClick = onClick@{
-              if (
-                config.passphrase.isEmpty() &&
-                  config.securityType != SoftApSecurityType.SECURITY_TYPE_OPEN
-              ) {
-                scope.launch {
-                  snackbarHostState.showSnackbar(
-                    message = passphraseEmptyWarningText,
-                    withDismissAction = true,
-                    duration = SnackbarDuration.Short,
-                  )
-                }
-                return@onClick
-              }
-              if (!vm.commit()) {
-                scope.launch {
-                  snackbarHostState.showSnackbar(
-                    message = failedToSaveText,
-                    withDismissAction = true,
-                    duration = SnackbarDuration.Short,
-                  )
-                }
-              } else {
-                scope.launch {
-                  snackbarHostState.showSnackbar(
-                    message = savedText,
-                    withDismissAction = true,
-                    duration = SnackbarDuration.Short,
-                  )
-                }
-              }
+        MaxClientLimitField(
+          maxClient = status.capabilities.maxSupportedClients,
+          onMaxClientChange = { vm.updateMaxClientLimit(it.toInt()) },
+          allowedLimit = config.maxClientLimit,
+        )
+      }
+      item {
+        MacRandomizationField(
+          macRandomizationSetting = config.macRandomizationSetting,
+          onSettingChange = { vm.updateMacRandomizationSetting(it) },
+        )
+      }
+      item {
+        AutoShutDownTimeOutField(
+          autoShutDownTimeOut =
+            config.autoShutdownTimeout.takeIf {
+              it in SoftApAutoShutdownTimeout.supportedShutdownTimeouts
+            } ?: SoftApAutoShutdownTimeout.DEFAULT,
+          supportedAutoShutdownType =
+            SoftApAutoShutdownTimeout.supportedShutdownTimeouts,
+          onAutoShutdownChange = { vm.updateAutoShutdownTimeout(it) },
+        )
+      }
+      item {
+        PresetField(
+          onShowPresets = { isPresetListShown = true },
+          onSaveConfig = { shouldSavePreset = true },
+        )
+      }
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        item { SoftApTileField() }
+      }
+    }
+    item {
+      Button(
+        onClick = onClick@{
+            if (
+              config.passphrase.isEmpty() &&
+                config.securityType != SoftApSecurityType.SECURITY_TYPE_OPEN
+            ) {
+              onShowSnackbar(passphraseEmptyWarningSnackbar)
+              return@onClick
             }
-        ) {
-          Text(text = stringResource(R.string.save_button))
-        }
+            onShowSnackbar(
+              if (vm.commit()) savedSnackbar else failedToSaveSnackbar
+            )
+          }
+      ) {
+        Text(text = stringResource(R.string.save_button))
       }
     }
   }
