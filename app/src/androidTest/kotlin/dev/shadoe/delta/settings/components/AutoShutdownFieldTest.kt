d64d49c1e5a04746992b65f3daed47cf1f243981
supershadoe <shadoe@shadoe.dev>
2025-05-03 13:59:57 +0530

    Formatting

--- app/src/androidTest/kotlin/dev/shadoe/delta/settings/components/AutoShutdownFieldTest.kt
+++ app/src/androidTest/kotlin/dev/shadoe/delta/settings/components/AutoShutdownFieldTest.kt
@@ -4,20 +4,19 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.test.assertIsOn
 import androidx.compose.ui.test.assertIsOff
-import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.assertIsOn
 import androidx.compose.ui.test.isToggleable
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.performClick
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import org.junit.Rule
 import kotlin.test.Test
+import org.junit.Rule
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class AutoShutdownFieldTest {
-  @get:Rule
-  val composeTestRule = createComposeRule()
+  @get:Rule val composeTestRule = createComposeRule()
 
   @Test
   fun `Check if the switch invokes the callback on tap`() {
